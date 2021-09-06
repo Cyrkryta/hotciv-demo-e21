e88888e1241c473e113206c0ac8907c91cf42238
Magnus Lindberg Christensen <202005458@post.au.dk>
2021-09-06 13:26:08 +0200

    First base code imported into git repo.

---
+++ src/test/java/hotciv/standard/TestAlphaCiv.java
@@ -0,0 +1,72 @@
+package hotciv.standard;
+
+import hotciv.framework.*;
+
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.*;
+
+/** Skeleton class for AlphaCiv test cases
+
+   Updated Aug 2020 for JUnit 5 includes
+   Updated Oct 2015 for using Hamcrest matchers
+
+   This source code is from the book 
+     "Flexible, Reliable Software:
+       Using Patterns and Agile Development"
+     published 2010 by CRC Press.
+   Author: 
+     Henrik B Christensen 
+     Department of Computer Science
+     Aarhus University
+   
+   Please visit http://www.baerbak.com/ for further information.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+ 
+       http://www.apache.org/licenses/LICENSE-2.0
+ 
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+public class TestAlphaCiv {
+  private Game game;
+
+  /** Fixture for alphaciv testing. */
+  @BeforeEach
+  public void setUp() {
+    game = new GameImpl();
+  }
+
+  // FRS p. 455 states that 'Red is the first player to take a turn'.
+
+  /************ TESTS FOR PLAYERS ************/
+  // Testing that Red is the first player to take turn.
+  @Test
+  public void shouldBeRedAsStartingPlayer() {
+    // Player in turn should be equal to red.
+    assertThat(game.getPlayerInTurn(), is(Player.RED));
+  }
+
+  /************ TESTS FOR WORLD LAYOUT ************/
+  // Testing that a tile returns a tile.
+  @Test
+  public void shouldGetTileReturnTile() {
+    assertThat(game.getTileAt(new Position(0,2)), is(notNullValue()));
+  }
+
+  // Testing that ocean tile is at position (0,1).
+  @Test
+  public void shouldBeOceanAt0Point1() {
+
+  }
+}
